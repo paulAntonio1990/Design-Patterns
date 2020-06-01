cd6d8b88579dcf50cedaaac9bea3f2530b4410e7
Paul <paulantonioifrmi@yahoo.com>
2020-06-01 16:28:21 +0300

    singletone thread safe

---
+++ src/com/exam/singletone/threadSafe/AgentieThreadSafe.java
@@ -0,0 +1,47 @@
+package com.exam.singletone.threadSafe;
+
+public class AgentieThreadSafe {
+    private String name;
+    private Double capital;
+    private Integer numarAngajati;
+
+    private static AgentieThreadSafe singletone;
+
+    public AgentieThreadSafe(String name, Double capital, Integer numarAngajati) {
+        this.name = name;
+        this.capital = capital;
+        this.numarAngajati = numarAngajati;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCapital() {
+        return capital;
+    }
+
+    public void setCapital(Double capital) {
+        this.capital = capital;
+    }
+
+    public Integer getNumarAngajati() {
+        return numarAngajati;
+    }
+
+    public void setNumarAngajati(Integer numarAngajati) {
+        this.numarAngajati = numarAngajati;
+    }
+
+    public static synchronized AgentieThreadSafe getSingletone(String name, Double capital, Integer numarAngajati) {
+        if (singletone == null) {
+            singletone = new AgentieThreadSafe(name, capital, numarAngajati);
+        }
+        return singletone;
+    }
+
+}
---
+++ src/com/exam/singletone/threadSafe/Main.java
@@ -0,0 +1,13 @@
+package com.exam.singletone.threadSafe;
+
+public class Main {
+    public static void main(String[] args) {
+        AgentieThreadSafe ag1 = AgentieThreadSafe.getSingletone("Agentie_1", 100.0, 100);
+        AgentieThreadSafe ag2 = AgentieThreadSafe.getSingletone("Agentie_2", 200.0, 200);
+
+        System.out.println(ag1.getName());
+        System.out.println(ag2.getName());
+        System.out.println(ag1);
+        System.out.println(ag2);
+    }
+}
